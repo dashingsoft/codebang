ee159bc41b3d1f5834becd518b3d00548b8048b7
Jondy Zhao <jondy.zhao@gmail.com>
2020-03-11 09:18:12 +0800

    Home and load

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,153 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header class="cb-brand">
+          <img src="./assets/logo.png">
+          <span>CodeBang</span>
+          <div class="cb-title">{{ title }}</div>
+      </el-header>
+      <div class="cb-toolbar">
+        <el-row>
+          <el-button type="info" size="mini" @click="handleNewFile">新建</el-button>
+    <el-button type="info" size="mini" @click="handleOpenFile">打开
+          <input type="file" accept=".c,.cpp,.h,.hpp" style="display:none"
+                 @change="handleFiles">
+
+</el-button>
+          <el-button type="info" size="mini" @click="handleSaveFile">保存</el-button>
+          <el-button type="info" size="mini" @click="handleBuildFile">编译</el-button>
+          <el-button type="info" size="mini" @click="handleRunFile">运行</el-button>
+        </el-row>
+      </div>
+      <div class="cb-main">
+        <div id="editor" class="cb-editor"></div>
+        <div class="cb-pixie hidden"></div>
+      </div>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ace from 'ace-builds';
+import 'ace-builds/webpack-resolver';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    data() {
+        return {
+            title: "新文件",
+            editor: null,
+            filename: '',
+        }
+    },
+    mounted() {
+        var main = this.$el.querySelector('.cb-main');
+        var rect = main.previousElementSibling.getBoundingClientRect();
+        main.style.height = (window.innerHeight - rect.bottom) + 'px';
+
+        this.editor = ace.edit("editor", {
+            mode: "ace/mode/c_cpp",
+            theme: "ace/theme/twilight",
+            // maxLines: 30,
+            // minLines: 10,
+            // readOnly: true,
+            fontSize: 18,
+            autoScrollEditorIntoView: true,
+            displayIndentGuides: false
+        });
+        // editor.renderer.setScrollMargin(10, 10, 10, 10);
+        this.editor.getSession().setUseWrapMode(true);
+        this.editor.focus();
+    },
+    components: {
+    },
+    methods: {
+        handleNewFile: function () {
+            this.editor.setValue(
+                '#include <stdio.h>\n\nint main(int argc, char *argv[])\n' +
+                    '{\n    printf("Hello World\\n");\n    return 0;\n}')
+            this.editor.selection.clearSelection()
+            this.editor.gotoLine(1);
+            this.editor.focus()
+        },
+        handleOpenFile: function () {
+            this.$el.querySelector('input[type="file"]').click()
+        },
+        handleFiles: function () {
+            var file = this.$el.querySelector('input[type="file"]').files[0];
+            this.title = file.name;
+            // this.title = file.webkitRelativePath;            
+            var reader = new FileReader();
+            reader.onload = function (evt) {
+                this.editor.setValue(evt.target.result);
+                this.editor.selection.clearSelection();
+                this.editor.gotoLine(1);
+                this.editor.focus();
+            }.bind(this);
+            reader.readAsText(file);
+        },
+        handleSaveFile: function () {
+        },
+        handleBuildFile: function () {
+        },
+        handleRunFile: function () {
+        }
+    }
 }
 </script>
 
 <style>
+html {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+}
+body {
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    width: 100%;
+    color: #f8f8f8;
+    background-color: #333;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.cb-brand {
+    display: inline-table;
+}
+.cb-brand img {
+    display: table-cell;
+    vertical-align: middle;
+    width: 32px;
+    height: 32px;
+    padding: 12px 6px;
+}
+.cb-brand span {
+    display: table-cell;
+    vertical-align: middle;
+}
+.cb-title {
+    display: table-cell;
+    width: 100%;
+    text-align: center;
+    vertical-align: middle;
+}
+.cb-toolbar {
+    padding: 0px 20px 9px 20px;
+}
+.cb-main {
+}
+.cb-editor {
+    width: 100%;
+    height: 100%;
+}
+.cb-pixie {
+    width: 100%;
+    height: 100%;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="hello">
+    <pre id="editor">function foo(items) {
+    var i;
+    for (i = 0; i &lt; items.length; i++) {
+        alert("Ace Rocks " + items[i]);
+    }
+}</pre>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -31,11 +37,60 @@
 </template>
 
 <script>
+import ace from 'ace-builds'
+// import "ace-builds/src-noconflict/theme-twilight";
+// import "ace-builds/src-noconflict/mode-javascript";
+// import "ace-builds/src-noconflict/worker-javascript";
+import 'ace-builds/webpack-resolver';
+import * as PIXI from 'pixi.js';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+    mounted() {
+        ace.edit("editor", {
+	mode: "ace/mode/javascript",
+	theme: "ace/theme/twilight",
+	maxLines: 50,
+	minLines: 10,
+	fontSize: 18
+        });
+
+        // The application will create a renderer using WebGL, if possible,
+        // with a fallback to a canvas render. It will also setup the ticker
+        // and the root stage PIXI.Container
+        const app = new PIXI.Application();
+
+        // The application will create a canvas element for you that you
+        // can then insert into the DOM
+        document.body.appendChild(app.view);
+
+        // load the texture we need
+        app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
+            // This creates a texture from a 'bunny.png' image
+            const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+            // Setup the position of the bunny
+            bunny.x = app.renderer.width / 2;
+            bunny.y = app.renderer.height / 2;
+
+            // Rotate around the center
+            bunny.anchor.x = 0.5;
+            bunny.anchor.y = 0.5;
+
+            // Add the bunny to the scene we are building
+            app.stage.addChild(bunny);
+
+            // Listen for frame updates
+            app.ticker.add(() => {
+                // each frame we spin the bunny around a bit
+                bunny.rotation += 0.01;
+            });
+        });
+        
+    },
 }
 </script>
 
