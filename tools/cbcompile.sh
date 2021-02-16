7e05f54ee452429354b5c5d7e91fbbeffc160083
Jondy Zhao <jondy.zhao@gmail.com>
2021-02-16 11:44:39 +0800

    Refine cbcompile

--- tools/cbcompile.sh
+++ tools/cbcompile.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
 src=$1
-dst=$2
+if [[ -z "$src" ]] ; then
+    read -p "Please type source path: " src
+fi
+
+if [[ -z "$src" || ! -f $src ]] ; then
+    echo "invalid source path: $src"
+    exit 1
+fi
+
+dst=$src.out
 GCC=aarch64-linux-gnu-gcc
 CBCC=./cbcc
 
-$CBCC $src -o $dst
-$GCC -o $dst/a.out $src
+# $CBCC $src -o $dst
+$GCC -g -o $dst $src
